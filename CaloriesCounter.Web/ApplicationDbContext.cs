7f7fc6eee3dcdd780dadd4655e9267aa9adad244
miliqika <maria555768@gmail.com>
2025-03-17 14:30:00 +0200

    AddedDBContext

---
+++ CaloriesCounter.Web/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CaloriesCounter.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ CaloriesCounter.Web/ApplicationDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Конфігурація
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Підключення до PostgreSQL
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
