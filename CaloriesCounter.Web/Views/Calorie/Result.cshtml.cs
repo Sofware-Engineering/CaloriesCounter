4617f0a540596ab2b43351a6b092e8884206e6dc
KhrystynaBroshko <khrystynabroshko@gmail.com>
2025-03-17 14:30:00 +0200

    Added Views Product and Calorie

---
+++ CaloriesCounter.Web/Views/Calorie/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaloriesCounter.Web.Views.Calorie
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
